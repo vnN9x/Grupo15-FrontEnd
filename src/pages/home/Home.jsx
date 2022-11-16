1b1c11bf96118b2a61d3d35d7aeecf13454a0282
Vinícius Maximiliano <vinnemaax@gmail.com>
2022-11-16 02:43:05 -0300

    Test request on home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,21 @@ import React from 'react'
 import TopBar from '../../components/TopBar/TopBar';
 import HomeSideBar from '../../components/homeSidebar/HomeSideBar'
 import Header from '../../components/header/Header';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom'
 
 export default function Home() {
+  const [locations, setLocations] = useState([]);
+  const {search} = useLocation();
+
+  useEffect(() =>{
+    const fetchLocations = async ()=>{
+      const res = await axios.get("/place"+search)
+      console.log(res)
+      setLocations(res.data)
+    }
+    fetchLocations()
   return (
     <>
         <div className='home'>
