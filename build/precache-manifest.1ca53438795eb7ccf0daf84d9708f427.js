248aadcb8e19658c048a962ef3340ed2f412facb
Vinícius Maximiliano <vinnemaax@gmail.com>
2022-11-16 01:34:24 -0300

    added build

---
+++ build/precache-manifest.1ca53438795eb7ccf0daf84d9708f427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "497f6bd2f140a6074003",
+    "url": "/static/css/main.6403250d.chunk.css"
+  },
+  {
+    "revision": "497f6bd2f140a6074003",
+    "url": "/static/js/main.497f6bd2.chunk.js"
+  },
+  {
+    "revision": "ee22c3c18f8301787a5d",
+    "url": "/static/js/1.ee22c3c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "924bda2a4b71341d57ff2256c3382c37",
+    "url": "/index.html"
+  }
+];
